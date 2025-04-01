29b437aa5b4da802042f6dd30911cb135b89ebcc
khalidkarimkqr <khalid13878@hotmail.com>
2025-04-01 09:56:09 -0700

    fixed the transition between pages to front or last page, so it shows all the pages being flipped around, less goo!!

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -246,16 +246,44 @@ const Page = ({number, front, back,page, opened,bookClosed, ...props}) => {
   
 export const Book = ({ ...props }) => {
     const [page] = useAtom(pageAtom);
+    const [delayedPage, setDelayedPage] = useState(page);
+    useEffect(() => {
+        let timeout;
+        const goToPage = () => {
+          setDelayedPage((delayedPage) => {
+            if (page === delayedPage) {
+              return delayedPage;
+            } else {
+              timeout = setTimeout(
+                () => {
+                  goToPage();
+                },
+                Math.abs(page - delayedPage) > 2 ? 50 : 150
+              );
+              if (page > delayedPage) {
+                return delayedPage + 1;
+              }
+              if (page < delayedPage) {
+                return delayedPage - 1;
+              }
+            }
+        });
+    };
+    goToPage();
+    return () => {
+        clearTimeout(timeout);
+      };
+    }, [page]);
     return (
       <group {...props} rotation-y={-Math.PI / 2}>
         {[...pages].map((pageData, index) => 
           
             <Page 
               key={index} 
-              page = {page}
+              page = {delayedPage}
               number={index} 
-              opened={page > index}
-              bookClosed={page===0 || page === pages.length}
+              opened={delayedPage > index}
+              bookClosed={delayedPage===0 || delayedPage === pages.length}
               {...pageData} 
             />
         )}
