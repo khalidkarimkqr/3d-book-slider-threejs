fb21990b98e26c6ff1e9540229f64688f290dd29
khalidkarimkqr <khalid13878@hotmail.com>
2025-04-01 04:43:29 -0700

    added a Page component with number fron tback and the props

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -1,7 +1,22 @@
+import {pages} from "./UI"
+
+const Page = ({number, front, back, ...props}) => {
+  return (
+    <group {...props}>
+      
+    </group>
+  )
+}
+
+
+
 export const Book = ({ ...props }) => {
-  return <group {...props}>
-    {[...pages].ap((pageData, index) => (
-      <Page key={index} number={index} {...pageData} />
-    ))}
-    </group>;
+  return (
+    <group {...props}>
+      {[...pages].map((pageData, index) => (
+        <Page key={index} number={index} {...pageData} />
+      ))}
+    </group>
+  )
 };
+
