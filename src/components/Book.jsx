34cbc2e325f794fa19e3f35777d31b521040db22
khalidkarimkqr <khalid13878@hotmail.com>
2025-04-01 04:45:57 -0700

    added a small box to see if its working

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -3,7 +3,11 @@ import {pages} from "./UI"
 const Page = ({number, front, back, ...props}) => {
   return (
     <group {...props}>
-      
+      <mesh scale = {0.1}>
+        <boxGeometry />
+        <meshBasicMaterial color= "red"/>
+      </mesh>
+
     </group>
   )
 }
