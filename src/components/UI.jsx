1c0f61d684cb9ad63d1b00a8df2b2909f0564e6c
khalidkarimkqr <khalid13878@hotmail.com>
2025-04-01 04:19:33 -0700

    modified the UI file to match it with my interests

--- src/components/UI.jsx
+++ src/components/UI.jsx
@@ -83,22 +83,20 @@ export const UI = () => {
         <div className="relative">
           <div className="bg-white/0  animate-horizontal-scroll flex items-center gap-8 w-max px-8">
             <h1 className="shrink-0 text-white text-10xl font-black ">
-              Wawa Sensei
+              Khalid Karim
             </h1>
             <h2 className="shrink-0 text-white text-8xl italic font-light">
-              React Three Fiber
+              Aspiring Software Developer
             </h2>
             <h2 className="shrink-0 text-white text-12xl font-bold">
               Three.js
             </h2>
             <h2 className="shrink-0 text-transparent text-12xl font-bold italic outline-text">
-              Ultimate Guide
+              Computer Science
             </h2>
-            <h2 className="shrink-0 text-white text-9xl font-medium">
-              Tutorials
-            </h2>
+            <h2 className="shrink-0 text-white text-9xl font-medium">Coding</h2>
             <h2 className="shrink-0 text-white text-9xl font-extralight italic">
-              Learn
+              is Fun
             </h2>
             <h2 className="shrink-0 text-white text-13xl font-bold">
               Practice
@@ -109,22 +107,20 @@ export const UI = () => {
           </div>
           <div className="absolute top-0 left-0 bg-white/0 animate-horizontal-scroll-2 flex items-center gap-8 px-8 w-max">
             <h1 className="shrink-0 text-white text-10xl font-black ">
-              Wawa Sensei
+              Khalid Karim
             </h1>
             <h2 className="shrink-0 text-white text-8xl italic font-light">
-              React Three Fiber
+              Aspiring Software Developer
             </h2>
             <h2 className="shrink-0 text-white text-12xl font-bold">
               Three.js
             </h2>
             <h2 className="shrink-0 text-transparent text-12xl font-bold italic outline-text">
-              Ultimate Guide
+              Computer Science
             </h2>
-            <h2 className="shrink-0 text-white text-9xl font-medium">
-              Tutorials
-            </h2>
+            <h2 className="shrink-0 text-white text-9xl font-medium">Coding</h2>
             <h2 className="shrink-0 text-white text-9xl font-extralight italic">
-              Learn
+              is Fun
             </h2>
             <h2 className="shrink-0 text-white text-13xl font-bold">
               Practice
